aad6a29847250e6c3ff6051d962ff24a35e736ec
kanalkun54 <kanalkun54@gmail.com>
2025-09-24 18:12:39 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ c = input(">>>: ")
 if c == "0":
     os.system("pip install aiohttp")
     os.system("pip install asyncio")
-    os.system("pip install requests")
-    os.system("pip install validators")
+    os.system("pip install aiohttp_socks")
     os.system("pip install colorama")
+    os.system("pip install numpy")
 elif c == "1":
     os.system("pip3 install aiohttp")
     os.system("pip3 install asyncio")
-    os.system("pip3 install requests")
-    os.system("pip3 install validators")
+    os.system("pip3 install aiohttp_socks")
     os.system("pip3 install colorama")
+    os.system("pip3 install numpy")
    
 print("Done.")
